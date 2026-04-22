2ca6f784ebec6e24d101101aaf54250f7e4237df
Mukhammad Hasan Muallif <muhamadh293@gmail.com>
2026-04-22 17:54:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ with st.sidebar:
         f"""<h2 style='text-align: center; letter-spacing: 2px; color: #FF4B4B; margin-top: -80px; margin-bottom: 0px;'> HEARTLYTIC <span style='color: var(--text-color); font-weight: bold;'>PRO</span></h2>
         """, unsafe_allow_html=True)
     st.markdown(
-        "<p style='text-align: center; font-size: 0.8rem; color: #888; margin-top: -10px;'>"
+        "<p style='text-align: center; font-size: 0.8rem; color: #888; margin-top: -70px;'>"
         "AI Cardiac Analytics</p>", 
         unsafe_allow_html=True)
     st.markdown("---")
